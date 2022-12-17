20c4bb11f5250a3885482d46c3d3f7ea3d3b93fc
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2022-12-18 00:35:35 +0100

    refactor

--- src/pages/ingredients/IngredientsPage.jsx
+++ src/pages/ingredients/IngredientsPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { IngredientDetails } from "../../components/IngredientDetails/IngredientDetails";
 import styles from "./IngredientsPage.module.css";
 
 export const Ingredients = () => {
-    return (
-        <div className={styles.content_box}>
-            <IngredientDetails />
-        </div>
-    )
-}
+  return (
+    <div className={styles.content_box}>
+      <IngredientDetails />
+    </div>
+  );
+};
