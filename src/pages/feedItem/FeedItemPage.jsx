6b4e510d9de2a31d015494a3aaaa9ef6d319c841
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2023-02-18 16:10:27 +0100

    edit pages profileorder and feeditem

--- src/pages/feedItem/FeedItemPage.jsx
+++ src/pages/feedItem/FeedItemPage.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 import { FeedDetails } from "../../components/FeedDetails/FeedDetails";
 import FeedItemPageStyles from "../feedItem/FeedItemPage.module.css";
+import {
+  wsConnectedStart,
+  wsConnectedClosed,
+} from "../../services/actions/wsActions";
 
 export const ItemOfFeed = () => {
+  const dispatch = useDispatch();
+  let socketConnect = useRef(false);
+
+  useEffect(() => {
+    if (!socketConnect.current) {
+      dispatch(wsConnectedStart());
+    }
+    return () => {
+      if (socketConnect.current) {
+        dispatch(wsConnectedClosed());
+      }
+      socketConnect.current = true;
+    }
+  }, []);
+
   return (
     <div className={FeedItemPageStyles.content_box}>
       <FeedDetails />
--- src/pages/profile/ProfileOrderPage.jsx
+++ src/pages/profile/ProfileOrderPage.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 import { OrderInfo } from "../../components/OrderInfo/OrderInfo";
 import FeedItemPageStyles from "../feedItem/FeedItemPage.module.css";
+import {
+  wsUserConnectedStart,
+  wsUserConnectedClosed,
+} from "../../services/actions/wsUserActions";
 
 export const ProfileOrderPage = () => {
+  const dispatch = useDispatch();
+
+  let socketConnect = useRef(false);
+
+  useEffect(() => {
+    if (!socketConnect.current) {
+      dispatch(wsUserConnectedStart());
+    }
+    return () => {
+      if (socketConnect.current) {
+        dispatch(wsUserConnectedClosed());
+      }
+      socketConnect.current = true;
+    };
+  }, []);
+
   return (
     <div className={FeedItemPageStyles.content_box}>
-       <OrderInfo />
+      <OrderInfo />
     </div>
   );
 };
