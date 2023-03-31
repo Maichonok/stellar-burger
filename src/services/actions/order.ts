22610e3b09bcd37c453f882bf312e64cea8204ff
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2023-03-31 18:09:26 +0200

    add types for burger ingredients

--- src/services/actions/order.ts
+++ src/services/actions/order.ts
@@ -1,12 +1,14 @@
+import { AppDispatch } from "../models";
 import { order as orderRequest } from "../../utils/api";
+import { TIngredient } from "../models/ingredients";
 
-export const ORDER_REQUEST = 'ORDER_REQUEST';
-export const ORDER_SUCCESS = 'ORDER_SUCCESS';
-export const ORDER_FAILURE = 'ORDER_FAILURE';
-export const ORDER_MODAL_OPEN = 'ORDER_MODAL_OPEN';
-export const ORDER_MODAL_CLOSE = 'ORDER_MODAL_CLOSE';
+export const ORDER_REQUEST: 'ORDER_REQUEST'  = 'ORDER_REQUEST';
+export const ORDER_SUCCESS: 'ORDER_SUCCESS' = 'ORDER_SUCCESS';
+export const ORDER_FAILURE: 'ORDER_FAILURE' = 'ORDER_FAILURE';
+export const ORDER_MODAL_OPEN: 'ORDER_MODAL_OPEN' = 'ORDER_MODAL_OPEN';
+export const ORDER_MODAL_CLOSE: 'ORDER_MODAL_CLOSE' = 'ORDER_MODAL_CLOSE';
 
-export const order = ingredients => dispatch => {
+export const order = (ingredients: Array<TIngredient>) => (dispatch: AppDispatch)  => {
     dispatch({
         type: ORDER_REQUEST
     });
